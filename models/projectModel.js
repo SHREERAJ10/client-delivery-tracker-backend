46de8ad417af40d784f7e694c989f6def30c09fd
Shreeraj Shrestha <shreerajshrestha.dev@gmail.com>
2026-02-13 19:19:17 +0545

    fix: typo in schema name

--- models/projectModel.js
+++ models/projectModel.js
@@ -1,6 +1,6 @@
 import * as z from 'zod';
 
-export const clientSchema = z.object({
+export const projectSchema = z.object({
     projectName: z.string().trim().min(1).max(50),
     clientId:z.uuid(),
     statusId: z.uuid(),
