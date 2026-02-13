8e1cd3089b25fe9ed05f8703bdb5939ad598a249
Shreeraj Shrestha <shreerajshrestha.dev@gmail.com>
2026-02-13 07:33:27 +0545

    add: nested project route

--- routes/client.route.js
+++ routes/client.route.js
@@ -1,8 +1,11 @@
 import express from 'express';
 import verifyToken from '../middlewares/verifyToken.js';
+import projectRouter from "../routes/project.route.js";
 import { createClient, deleteClient, getClient, updateClient } from '../controllers/client.controller.js';
 
 const router = express.Router();
+
+router.use('/project', projectRouter);
 
 router.post('/', verifyToken, createClient);
 router.get('/',verifyToken, getClient);
