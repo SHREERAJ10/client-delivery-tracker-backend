8e59b6c96ac3448959e81418224514bb18a6cf91
Shreeraj Shrestha <shreerajshrestha.dev@gmail.com>
2026-02-14 21:43:59 +0545

    feat: get paginated list of deliverables of client's project

--- controllers/deliverable.controller.js
+++ controllers/deliverable.controller.js
@@ -1,5 +1,6 @@
 import { deliverableSchema } from "../models/deliverableModel.js";
 import * as deliverableService from "../services/deliverable.service.js";
+import { paginate } from "../utils/paginate.js";
 
 export const createDeliverable = async (req, res) => {
   try {
@@ -14,6 +15,30 @@ export const createDeliverable = async (req, res) => {
     res
       .status(200)
       .json({ success: true, message: "Deliverable Created Successfully!" });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ success: false, error: "internal server error!" });
+  }
+};
+
+export const getDeliverable = async (req, res) => {
+  const pageSize = 10;
+  const modelName = "deliverable";
+  const { projectId } = req.params;
+  try {
+    const currPage = req.query.page || 1;
+    const deliverables = await paginate(
+      currPage,
+      pageSize,
+      modelName,
+      {
+        projectId: projectId,
+      },
+      {
+        status: true,
+      },
+    );
+    res.status(200).json({ success: true, deliverables: deliverables });
   } catch (err) {
     console.log(err);
     res.status(500).json({ success: false, error: "internal server error!" });
--- routes/deliverable.route.js
+++ routes/deliverable.route.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import verifyToken from '../middlewares/verifyToken.js';
-import { createDeliverable } from '../controllers/deliverable.controller.js';
+import { createDeliverable, getDeliverable } from '../controllers/deliverable.controller.js';
 
 const router = express.Router({mergeParams:true});
 
 router.post("/", verifyToken, createDeliverable);
+router.get("/", verifyToken, getDeliverable);
 
 export default router;
