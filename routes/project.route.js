fb93ab11b9ae1d7b35dfb5f643316d63a8b2df84
Shreeraj Shrestha <shreerajshrestha.dev@gmail.com>
2026-02-14 21:17:56 +0545

    add: nested deliverable route

--- routes/project.route.js
+++ routes/project.route.js
@@ -1,8 +1,11 @@
 import express from 'express';
 import verifyToken from '../middlewares/verifyToken.js';
 import { createProject, deleteProject, getProject, updateProject } from '../controllers/project.controller.js';
+import deliverableRouter from "../routes/deliverable.route.js";
 
 const router = express.Router({mergeParams:true});
+
+router.use('/:projectId/deliverable', deliverableRouter)
 
 router.post('/', verifyToken, createProject);
 router.get('/', verifyToken, getProject);
