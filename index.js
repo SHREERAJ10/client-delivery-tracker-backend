cc50c639225a4ce1bc272b884b8f302d13342883
Shreeraj Shrestha <shreerajshrestha.dev@gmail.com>
2026-03-06 22:22:46 +0545

    add: dashboard route

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 import express from "express";
 import clientRouter from "./routes/client.route.js";
+import dashboardRouter from "./routes/dashboard.route.js";
 
 const app = express();
 
 app.use(express.json());
 
 app.use("/client", clientRouter);
+app.use("/dashboard", dashboardRouter);
 
 app.listen(3000, () => {
   console.log("server listening on port 3000");
