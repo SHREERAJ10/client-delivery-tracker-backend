57c1e450484509934f8ece4a91aaa5cdf7ea431d
Shreeraj Shrestha <shreerajshrestha.dev@gmail.com>
2026-03-07 15:57:43 +0545

    add: deliverables due this week counter function

--- services/dashboard.service.js
+++ services/dashboard.service.js
@@ -4,12 +4,7 @@ const prisma = new PrismaClient();
 
 export const getTotalClients = async () => {
   const totalClients = await prisma.client.count();
-  const result = {
-    label: "Total Clients",
-    name: "totalClients",
-    count: totalClients,
-  };
-  return result;
+  return totalClients;
 };
 
 export const getActiveProjects = async () => {
@@ -22,10 +17,24 @@ export const getActiveProjects = async () => {
     },
   });
 
-  const result = {
-    label: "Active Projects",
-    name: "activeProjects",
-    count: activeProjects,
-  };
-  return result;
+  return activeProjects;
 };
+
+export const getDeliverablesDueThisWeekCount = async ()=>{
+  
+  const currDate = new Date();
+  const firstDayOfCurrWeek = currDate.getDate() - currDate.getDay();
+  const firstDate = new Date(currDate.setDate(firstDayOfCurrWeek));
+  const lastDate = new Date(currDate.setDate(firstDayOfCurrWeek + 6));
+
+  const dueThisWeekCount = await prisma.deliverable.count({
+    where:{
+      due_Date:{
+        gt:firstDate,
+        lt:lastDate,
+      }
+    }
+  });
+
+  return dueThisWeekCount;
+}
