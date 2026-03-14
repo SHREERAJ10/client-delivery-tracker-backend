e357fa17c645021cbed7785bfa6b30659e19d8ed
Shreeraj Shrestha <shreerajshrestha.dev@gmail.com>
2026-03-14 13:46:01 +0545

    feat: get paginated data for clients page

--- controllers/client.controller.js
+++ controllers/client.controller.js
@@ -2,14 +2,128 @@ import { Prisma } from "@prisma/client";
 import { clientSchema } from "../models/clientModel.js";
 import * as clientService from "../services/client.service.js";
 import { paginate } from "../utils/paginate.js";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+const groupByAndCount = async (model, by, where) => {
+  return await prisma[model].groupBy({
+    by: by,
+    where: where,
+    _count: {
+      id: true,
+    },
+  });
+};
+
+const deliverableCountPerClient = async (projects, where) => {
+  const deliverableCountPerProject = await groupByAndCount(
+    "deliverable",
+    "projectId",
+    where,
+  );
+
+  const projectToClientMap = {};
+  let clientToDeliverableCountMap = {};
+
+  for (let project of projects) {
+    projectToClientMap[project.id] = project.clientId;
+  }
+
+  for (let deliverable of deliverableCountPerProject) {
+    if (Object.keys(projectToClientMap).includes(deliverable.projectId)) {
+      const clientId = projectToClientMap[deliverable.projectId];
+      if (Object.hasOwn(clientToDeliverableCountMap, clientId)) {
+        clientToDeliverableCountMap[clientId] += deliverable._count.id;
+      } else {
+        clientToDeliverableCountMap[clientId] = deliverable._count.id;
+      }
+    }
+  }
+
+  return clientToDeliverableCountMap;
+};
 
 export const getClient = async (req, res) => {
-  const pageSize = 10;
+  const pageSize = 5;
   const modelName = "client";
   try {
     const currPage = req.query.page || 1;
-    const clients = await paginate(currPage, pageSize, modelName, {}, {});
-    res.status(200).json({ success: true, clientData: clients });
+
+    const clients = (
+      await paginate(
+        currPage,
+        pageSize,
+        modelName,
+        {},
+        {
+          id: true,
+          name: true,
+          email: true,
+        },
+      )
+    ).items;
+
+    const clientIds = clients.map((c) => c.id); //array of ids of clients, paginated
+
+    const projects = await prisma.project.findMany({
+      where: {
+        clientId: { in: clientIds },
+      },
+      select: {
+        id: true,
+        clientId: true,
+      },
+    });
+
+    const projectIds = projects.map((p) => p.id);
+
+    const activeProjectArray = await groupByAndCount("project", "clientId", {
+      status: {
+        status: "Active",
+      },
+      clientId: { in: clientIds },
+    });
+
+    const activeProjectCount = {};
+
+    for (let project of activeProjectArray) {
+      activeProjectCount[project.clientId] = project._count.id;
+    }
+
+    const overdueDeliverableCount = await deliverableCountPerClient(projects, {
+      due_Date: {
+        lt: new Date(),
+      },
+    });
+
+    const openDeliverableCount = await deliverableCountPerClient(projects, {
+      status: {
+        status: {
+          not: "Delivered",
+        },
+      },
+      projectId: { in: projectIds },
+    });
+
+    const response = clients.map((client) => {
+      if (Object.hasOwn(activeProjectCount, client.id)) {
+        client.project = {
+          active: activeProjectCount[client.id],
+        };
+      }
+      if (Object.hasOwn(overdueDeliverableCount, client.id)) {
+        client.deliverable = {
+          overdue: overdueDeliverableCount[client.id],
+        };
+      }
+      if (Object.hasOwn(openDeliverableCount, client.id)) {
+        client.deliverable.open = openDeliverableCount[client.id];
+      }
+      return client;
+    });
+
+    res.status(200).json({ success: true, data: response });
   } catch (err) {
     console.log(err);
     res.status(500).json({ success: false, error: "internal server error!" });
