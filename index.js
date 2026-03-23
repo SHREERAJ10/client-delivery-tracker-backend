90fa5df094f8710b3de8f829b9edb629c4ba18b5
Shreeraj Shrestha <shreerajshrestha.dev@gmail.com>
2026-03-23 17:56:57 +0545

    add: cors

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 import express from "express";
 import clientRouter from "./routes/client.route.js";
 import dashboardRouter from "./routes/dashboard.route.js";
+import cors from 'cors';
 
 const app = express();
 
 app.use(express.json());
-
+app.use(cors({
+    origin:"http://localhost:5173"
+}))
 app.use("/client", clientRouter);
 app.use("/dashboard", dashboardRouter);
 
