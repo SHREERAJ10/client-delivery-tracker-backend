2ce82c3ac229394c043c8eceeec56a0e4774971b
Shreeraj Shrestha <shreerajshrestha.dev@gmail.com>
2026-05-03 10:49:46 +0545

    feat(util): include page size in pagination response

--- utils/paginate.js
+++ utils/paginate.js
@@ -1,24 +1,25 @@
-import {PrismaClient} from '@prisma/client';
+import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-export const paginate = async (page, pageSize, modelName, where, select)=>{
-        const db = prisma[modelName];
-        const skip = (+page - 1) * +pageSize;
-        const totalCount = await db.count({
-            where
-        });
-        const items = await db.findMany({
-            where: where || {},
-            select: select || {},
-            skip: skip,
-            take: pageSize,
-        });
+export const paginate = async (page, pageSize, modelName, where, select) => {
+  const db = prisma[modelName];
+  const skip = (+page - 1) * +pageSize;
+  const totalCount = await db.count({
+    where,
+  });
+  const items = await db.findMany({
+    where: where || {},
+    select: select || {},
+    skip: skip,
+    take: pageSize,
+  });
 
-        return {
-            items,
-            totalCount,
-            currentPage: page,
-            previousPage: page - 1,
-        }
-}
+  return {
+    items,
+    totalCount,
+    pageSize: pageSize,
+    currentPage: page,
+    previousPage: page - 1,
+  };
+};
